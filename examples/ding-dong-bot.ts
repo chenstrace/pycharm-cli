46d6848022fa6041c6684d8d0c3f1bec89e20c5e
Your Name <you@example.com>
2024-05-15 13:26:43 +0800

    ding

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -1,94 +1,159 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
-/**
- * Wechaty - Conversational RPA SDK for Chatbot Makers.
- *  - https://github.com/wechaty/wechaty
- */
-// https://stackoverflow.com/a/42817956/1123955
-// https://github.com/motdotla/dotenv/issues/89#issuecomment-587753552
+
 import 'dotenv/config.js'
 
 import {
-  Contact,
-  Message,
-  ScanStatus,
-  WechatyBuilder,
-  log,
-}                  from 'wechaty'
+    Contact,
+    Message,
+    ScanStatus,
+    WechatyBuilder,
+    log,
+} from 'wechaty'
 
 import qrcodeTerminal from 'qrcode-terminal'
+import readline from 'readline';
+import type { ContactInterface } from 'wechaty/impls';
 
-function onScan (qrcode: string, status: ScanStatus) {
-  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    const qrcodeImageUrl = [
-      'https://wechaty.js.org/qrcode/',
-      encodeURIComponent(qrcode),
-    ].join('')
-    log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Choose a number: ',
+});
 
-    qrcodeTerminal.generate(qrcode, { small: true })  // show qrcode on console
+const aliasList = ['victor', 'moon', 'zyb', 'mm', 'bb'];
+let currentAliasIndex: number | null = null;
 
-  } else {
-    log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
-  }
+
+let targetContact: ContactInterface | null | undefined = null;
+const contactCache = new Map<string, ContactInterface>();
+
+
+function onScan(qrcode: string, status: ScanStatus) {
+    if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+        const qrcodeImageUrl = [
+            'https://wechaty.js.org/qrcode/',
+            encodeURIComponent(qrcode),
+        ].join('')
+        log.error('scan', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+
+        qrcodeTerminal.generate(qrcode, { small: true })
+    } else {
+        log.error('scan', 'onScan: %s(%s)', ScanStatus[status], status)
+    }
 }
 
-function onLogin (user: Contact) {
-  log.info('StarterBot', '%s login', user)
+function onLogin(user: Contact) {
+    log.error('onIn', '%s in', user)
 }
 
-function onLogout (user: Contact) {
-  log.info('StarterBot', '%s logout', user)
+function onLogout(user: Contact) {
+    log.error('onOut', '%s out', user)
 }
 
-async function onMessage (msg: Message) {
-  log.info('StarterBot', msg.toString())
-
-  if (msg.text() === 'ding') {
-    await msg.say('dong')
-  }
+async function onMessage(msg: Message) {
+    const room = msg.room()
+    if (room) {
+        return
+    }
+    const contact = msg.talker()
+    if (!contact || contact.type() !== bot.Contact.Type.Individual) {
+        return
+    }
+    log.error('M', 'n(%s),m(%s)', contact.name(), msg.text())
 }
 
-const bot = WechatyBuilder.build({
-  name: 'ding-dong-bot',
-  /**
-   * You can specific `puppet` and `puppetOptions` here with hard coding:
-   *
-  puppet: 'wechaty-puppet-wechat',
-  puppetOptions: {
-    uos: true,
-  },
-   */
-  /**
-   * How to set Wechaty Puppet Provider:
-   *
-   *  1. Specify a `puppet` option when instantiating Wechaty. (like `{ puppet: 'wechaty-puppet-whatsapp' }`, see below)
-   *  1. Set the `WECHATY_PUPPET` environment variable to the puppet NPM module name. (like `wechaty-puppet-whatsapp`)
-   *
-   * You can use the following providers locally:
-   *  - wechaty-puppet-wechat (web protocol, no token required)
-   *  - wechaty-puppet-whatsapp (web protocol, no token required)
-   *  - wechaty-puppet-padlocal (pad protocol, token required)
-   *  - etc. see: <https://wechaty.js.org/docs/puppet-providers/>
-   */
-  // puppet: 'wechaty-puppet-whatsapp'
+async function onReady() {
+    log.error('onReady', 'Ready Now');
 
-  /**
-   * You can use wechaty puppet provider 'wechaty-puppet-service'
-   *   which can connect to remote Wechaty Puppet Services
-   *   for using more powerful protocol.
-   * Learn more about services (and TOKEN) from https://wechaty.js.org/docs/puppet-services/
-   */
-  // puppet: 'wechaty-puppet-service'
-  // puppetOptions: {
-  //   token: 'xxx',
-  // }
-})
+    aliasList.forEach((alias, index) => {
+        console.error(`${index + 1}. ${alias}`);
+    });
 
-bot.on('scan',    onScan)
-bot.on('login',   onLogin)
-bot.on('logout',  onLogout)
-bot.on('message', onMessage)
 
-bot.start()
-  .then(() => log.info('StarterBot', 'Starter Bot Started.'))
-  .catch(e => log.error('StarterBot', e))
+    rl.setPrompt('Choose a number: ');
+
+    rl.prompt();
+    rl.on('line', handleMessage).on('close', handleClose);
+}
+
+async function findContactByAlias(alias: string): Promise<ContactInterface | null> {
+    const contact = await bot.Contact.find({ alias: alias });
+    return contact || null; // Explicitly return null if contact is not found (undefined)
+}
+
+async function handleMessage(line: string) {
+    try {
+
+    }
+    catch (ee) {
+
+    }
+    line = line.trim();
+
+    if (line === 'exit') {
+        rl.close();
+        return;
+    }
+
+    if (currentAliasIndex === null) {
+        const choice = parseInt(line);
+        if (isNaN(choice) || choice < 1 || choice > aliasList.length) {
+            log.error('OOM', 'Invalid choice. Please try again.');
+            rl.setPrompt('Choose a number: ');
+            rl.prompt();
+            return;
+        }
+
+        currentAliasIndex = choice - 1;
+
+        let alias = aliasList[currentAliasIndex] as string;
+
+        if (contactCache.has(alias)) {
+            targetContact = contactCache.get(alias);
+        } else {
+            targetContact = await findContactByAlias(alias);
+            if (targetContact) {
+                contactCache.set(alias, targetContact);
+            } else {
+                log.error('OOM', `Contact not found: ${aliasList[currentAliasIndex]}`);
+                currentAliasIndex = null;
+                rl.setPrompt('Choose a number: ');
+                rl.prompt();
+                return;
+            }
+        }
+
+        rl.setPrompt(`In(${aliasList[currentAliasIndex]}):`)
+        rl.prompt();
+    } else {
+        if (targetContact) {
+            const success_msg = "Success to " + aliasList[currentAliasIndex] + ":" + line;
+            targetContact.say(line)
+                .then(() => log.error('OOM', success_msg))
+                .catch(e => log.error('OOM', 'Failed to :', e));
+        }
+
+        currentAliasIndex = null;
+        aliasList.forEach((alias, index) => {
+            console.error(`${index + 1}. ${alias}`);
+        });
+        rl.setPrompt('Choose a number: ');
+        rl.prompt();
+    }
+}
+
+function handleClose() {
+    log.error('handleClose', 'Exiting message input mode.');
+}
+
+const bot = WechatyBuilder.build({ name: 'ding-dong-bot' });
+
+bot.on('scan', onScan)
+    .on('login', onLogin)
+    .on('logout', onLogout)
+    .on('message', onMessage)
+    .on('ready', onReady)
+    .on('error', console.error)
+    .start()
+    .then(() => log.error('StarterBot', 'Starter Bot Started.'))
+    .catch(e => log.error('handleException', e));
